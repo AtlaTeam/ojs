aeb3a2cd70f2b0ecdf9610bb63321638290a2449
Patrick Buss <patrick@Patricks-MacBook-Pro.local>
2022-10-19 11:55:57 -0500

    Add atla-other child theme

---
+++ plugins/themes/atla-other-bootstrap/index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @file plugins/themes/atla-other-bootstrap/index.php
+ *
+ * Copyright (c) 2021 Atla
+ *
+ * @brief Wrapper for the Atla Booststrap3 child theme.
+ */
+require_once('AtlaOtherBootstrapThemePlugin.inc.php');
+return new AtlaOtherBootstrapThemePlugin();
